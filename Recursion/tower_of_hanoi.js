302337e1657e23dbd94882bf581edcbd92465ef0
Siddharth Sharma <hearsid@gmail.com>
2022-12-30 07:22:55 +0530

    Update tower_of_hanoi.js

--- Recursion/tower_of_hanoi.js
+++ Recursion/tower_of_hanoi.js
@@ -1,15 +1,18 @@
-
-function towerOfHanoi(n, from_rod, to_rod, aux_rod) {
-    if (n == 0) {
+const diskColorMap = {
+    1: 'Red', 
+    2: 'Green',
+    3: 'Blue',
+    4: 'Gray'
+};
+function towerOfHanoi(n, sourceTower, destinationTower, auxiliaryTower) {
+    if (n == 0)
         return;
-    }
-    towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
-    console.log(`Move disk ${n} from rod ${from_rod} to rod ${to_rod}`);
-    towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
+    
+    towerOfHanoi(n-1, sourceTower, auxiliaryTower, destinationTower);
+    console.log(`${stepCount++}) Move ${diskColorMap[n]} disk from ${sourceTower} to ${destinationTower}`);
+    towerOfHanoi(n-1, auxiliaryTower, destinationTower, sourceTower);
 }
+const noOfDisks = 4;
+let stepCount = 1;
+towerOfHanoi(noOfDisks, 'A', 'C', 'B');
 
-// Driver code
-var N = 3;
-
-// A, B and C are names of rods
-towerOfHanoi(N, 'A', 'C', 'B');
